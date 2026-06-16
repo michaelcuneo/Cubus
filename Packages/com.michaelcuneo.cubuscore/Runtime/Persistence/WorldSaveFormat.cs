f85556e74fe19e762fe180c6fae4225c50c2fe05
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-16 22:11:43 +1000

    Add world save format constants

---
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Persistence/WorldSaveFormat.cs
@@ -0,0 +1,13 @@
+namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Persistence
+{
+  public static class WorldSaveFormat
+  {
+    public const int CurrentWorldSaveVersion = 4;
+    public const int CurrentBlockWorldSaveVersion = 1;
+    public const int CurrentDatabaseVersion = 1;
+
+    public const int MinimumSupportedWorldSaveVersion = 1;
+    public const int MinimumSupportedBlockWorldSaveVersion = 1;
+    public const int MinimumSupportedDatabaseVersion = 1;
+  }
+}
