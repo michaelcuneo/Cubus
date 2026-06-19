a5ef5a3251f53e81924747e23ff364cf61fec598
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-19 12:59:25 +1000

    Add missing world namespace import

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/DensityChunkBuildQueue.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/DensityChunkBuildQueue.cs
@@ -5,6 +5,7 @@ using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Meshing;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Terrain;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Voxels;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Chunks;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.World;
 using UnityEngine;
 
 namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
