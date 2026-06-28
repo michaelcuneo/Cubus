f93951967d5f4d338df9013ee166b50a2def1e44
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-28 20:21:34 +1000

    Make chunk load queue state thread safe

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/ChunkLoadQueue.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/ChunkLoadQueue.cs
@@ -17,8 +17,27 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
     private int activeTaskCount;
     private int generationId;
 
-    public int ActiveTaskCount => activeTaskCount;
-    public int GenerationId => generationId;
+    public int ActiveTaskCount
+    {
+      get
+      {
+        lock (stateLock)
+        {
+          return activeTaskCount;
+        }
+      }
+    }
+
+    public int GenerationId
+    {
+      get
+      {
+        lock (stateLock)
+        {
+          return generationId;
+        }
+      }
+    }
 
     public bool IsInFlight(Vector3Int chunkCoord)
     {
@@ -30,9 +49,12 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
 
     public void IncrementGeneration()
     {
-      generationId++;
-      activeTaskCount = 0;
-      inFlightChunkCoords.Clear();
+      lock (stateLock)
+      {
+        generationId++;
+        activeTaskCount = 0;
+        inFlightChunkCoords.Clear();
+      }
 
       lock (completedResults)
       {
@@ -48,13 +70,17 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
         IReadOnlyDictionary<int, ushort> blockOverrides = null,
         IReadOnlyDictionary<int, DensityVoxelOverride> densityOverrides = null)
     {
+      int requestGeneration;
 
-      if (activeTaskCount >= maxActiveTasks) return false;
-      if (inFlightChunkCoords.Contains(chunkCoord)) return false;
+      lock (stateLock)
+      {
+        if (activeTaskCount >= maxActiveTasks) return false;
+        if (inFlightChunkCoords.Contains(chunkCoord)) return false;
 
-      int requestGeneration = generationId;
-      inFlightChunkCoords.Add(chunkCoord);
-      activeTaskCount++;
+        requestGeneration = generationId;
+        inFlightChunkCoords.Add(chunkCoord);
+        activeTaskCount++;
+      }
 
       _ = Task.Run(() => Load(store, worldId, chunkCoord, requestGeneration, blockOverrides, densityOverrides)).ContinueWith(task =>
       {
@@ -75,8 +101,11 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
           completedResults.Enqueue(result);
         }
 
-        activeTaskCount = Mathf.Max(0, activeTaskCount - 1);
-        inFlightChunkCoords.Remove(chunkCoord);
+        lock (stateLock)
+        {
+          activeTaskCount = Mathf.Max(0, activeTaskCount - 1);
+          inFlightChunkCoords.Remove(chunkCoord);
+        }
       });
 
       return true;
