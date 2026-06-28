2057356e9ee871a5a991558fa8b3d5e1ebd929bc
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-29 01:02:01 +1000

    Enable block and density capabilities in hybrid mode

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.TerrainCapabilities.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.TerrainCapabilities.cs
@@ -7,14 +7,19 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
     private bool IsAnyTerrainEnabled =>
       IsBlockTerrainEnabled || IsDensityTerrainEnabled;
 
+    private bool IsHybridTerrainEnabled =>
+      world != null &&
+      world.Settings != null &&
+      world.Settings.TerrainSystem == TerrainSystem.Hybrid;
+
     private bool IsBlockTerrainEnabled =>
       world != null &&
       world.Settings != null &&
-      world.Settings.TerrainSystem == TerrainSystem.Block;
+      (world.Settings.TerrainSystem == TerrainSystem.Block || world.Settings.TerrainSystem == TerrainSystem.Hybrid);
 
     private bool IsDensityTerrainEnabled =>
       world != null &&
       world.Settings != null &&
-      world.Settings.TerrainSystem == TerrainSystem.SmoothDensity;
+      (world.Settings.TerrainSystem == TerrainSystem.SmoothDensity || world.Settings.TerrainSystem == TerrainSystem.Hybrid);
   }
 }
