4f16b2eb5d11b506619d97ebaa47f7dfbeea5e00
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-30 17:41:15 +1000

    Fix block build queue imports

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/BlockChunkBuildQueue.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/BlockChunkBuildQueue.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Chunks;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Meshing;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Terrain;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.World;
 using UnityEngine;
 
 namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
