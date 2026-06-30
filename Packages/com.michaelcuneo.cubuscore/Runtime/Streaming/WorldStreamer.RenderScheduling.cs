b94a7192a8f7c873afba0e85c2e1c8c383938ff5
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-30 17:30:36 +1000

    Restore density sample dependency gate

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.RenderScheduling.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.RenderScheduling.cs
@@ -136,6 +136,12 @@ namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
         return false;
       }
 
+      if (!EnsureDensitySampleChunksAvailableForMesh(c))
+      {
+        QueueDensityRender(c, false);
+        return false;
+      }
+
       int totalActiveTasks = chunkLoadQueue.ActiveTaskCount + buildQueue.ActiveTaskCount + densityBuildQueue.ActiveTaskCount;
 
       if (densityBuildQueue.ActiveTaskCount >= MaxDensityAsyncTasks || totalActiveTasks >= MaxTotalAsyncTasks)
