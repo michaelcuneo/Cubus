df5e2ac5b5493717aabd94b943dda21057d065c4
Michael Cuneo <michaelcuneo75@gmail.com>
2026-06-30 17:52:45 +1000

    Fix block meshing bounds extension import

--- Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.BlockMeshing.cs
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Streaming/WorldStreamer.BlockMeshing.cs
@@ -1,5 +1,6 @@
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Chunks;
 using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Meshing;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.World;
 using UnityEngine;
 
 namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming
