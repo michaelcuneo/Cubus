88740f9cacee4c9a89357abc274074e35195cc67
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-20 14:08:20 +1000

    Add streamed voxel water surfaces

---
+++ Packages/com.michaelcuneo.cubuscore/Runtime/Water/VoxelWaterSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Core;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Streaming;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Terrain;
+using CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.World;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CubusCore.Packages.com.michaelcuneo.cubuscore.Runtime.Water
+{
+  /// <summary>
+  /// First-pass static voxel water renderer.
+ 
